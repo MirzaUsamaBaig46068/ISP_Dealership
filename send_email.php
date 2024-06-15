3919a2e39bba948428b179180ba7a0bd1ffe0916
mirzausama512 <147047684+mirzausama512@users.noreply.github.com>
2024-06-15 16:53:20 +0500

    Email sending file has been renamed

---
+++ send_email.php
@@ -0,0 +1,236 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';  // Adjust the path to autoload.php if necessary
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $source = $_POST['source'];
+    $text = $_POST['text'];
+
+    /*if (empty($name) || empty($email) || empty($phone) || empty($source) || empty($text)) {
+        echo "All fields are required.";
+        exit;
+    }*/
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host       = 'mail.directstreamone.com';//getenv('SMTP_HOST');
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'team@directstreamone.com';//getenv('SMTP_USERNAME');
+        $mail->Password   = 'r&DsRB1N0HwD';//getenv('SMTP_PASSWORD');
+        $mail->SMTPSecure = 'tls';//getenv('SMTP_SECURE');
+        $mail->Port       = '587';//getenv('SMTP_PORT');
+        $mail->SMTPOptions = array(
+                                  'ssl' => array(
+                                  'verify_peer' => false,
+                                  'verify_peer_name' => false,
+                                  'allow_self_signed' => true
+    )
+);
+        
+        // Recipients
+        $mail->setFrom('team@directstreamone.com', 'Support Team');//getenv('SMTP_USERNAME');,getenv('SMTP_HOSTNAME');
+        $mail->addAddress($email, $name);
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = "<html><body>
+                          <h2>Contact Form Submission</h2>
+                          <p><strong>Name:</strong> " . htmlspecialchars($name) . "</p>
+                          <p><strong>Email:</strong> " . htmlspecialchars($email) . "</p>
+                          <p><strong>Phone:</strong> " . htmlspecialchars($phone) . "</p>
+                          <p><strong>Source:</strong> " . htmlspecialchars($source) . "</p>
+                          <p><strong>Message:</strong> " . nl2br(htmlspecialchars($text)) . "</p>
+                          </body></html>";
+      //   $mail->Body = '<!-- © 2018 Shift Technologies. All rights reserved. -->
+      // <table border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout:fixed;background-color:#f9f9f9" id="bodyTable">
+      //   <tbody>
+      //     <tr>
+      //       <td style="padding-right:10px;padding-left:10px;" align="center" valign="top" id="bodyCell">
+      //         <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperWebview" style="max-width:600px">
+      //           <tbody>
+      //             <tr>
+      //               <td align="center" valign="top">
+      //                 <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      //                   <tbody>
+      //                       </td>
+      //                     </tr>
+      //                   </tbody>
+      //                 </table>
+      //               </td>
+      //             </tr>
+      //           </tbody>
+      //         </table>
+      //         <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperBody" style="max-width:600px">
+      //           <tbody>
+      //             <tr>
+      //               <td align="center" valign="top">
+      //                 <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableCard" style="background-color:#fff;border-color:#e5e5e5;border-style:solid;border-width:0 1px 1px 1px;">
+      //                   <tbody>
+      //                     <tr>
+      //                       <td style="background-color:#00d2f4;font-size:1px;line-height:3px" class="topBorder" height="3">&nbsp;</td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding-top: 60px; padding-bottom: 20px;" align="center" valign="middle" class="emailLogo">
+      //                         <a href="#" style="text-decoration:none" target="_blank">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/hero-img/blue/logo.png" style="width:100%;max-width:150px;height:auto;display:block" width="150">
+      //                         </a>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding-bottom: 20px;" align="center" valign="top" class="imgHero">
+      //                         <a href="#" style="text-decoration:none" target="_blank">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/hero-img/blue/heroGradient/user-account.png" style="width:100%;max-width:600px;height:auto;display:block;color: #f9f9f9;" width="600">
+      //                         </a>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
+      //                         <h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Hi " . htmlspecialchars($name) . "</h2>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding-bottom: 30px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="subTitle">
+      //                         <h4 class="text" style="color:#999;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:16px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:24px;text-transform:none;text-align:center;padding:0;margin:0">Verify Your Email Account</h4>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding-left:20px;padding-right:20px" align="center" valign="top" class="containtTable ui-sortable">
+      //                         <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableDescription" style="">
+      //                           <tbody>
+      //                             <tr>
+      //                               <td style="padding-bottom: 20px;" align="center" valign="top" class="description">
+      //                                 <p class="text" style="color:#666;font-family:"Open Sans",Helvetica,Arial,sans-serif;font-size:14px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:22px;text-transform:none;text-align:center;padding:0;margin:0">Thanks for subscribe for the Vespro newsletter. Please click confirm button for subscription to start receiving our emails.</p>
+      //                               </td>
+      //                             </tr>
+      //                           </tbody>
+      //                         </table>
+      //                         <table border="0" cellpadding="0" cellspacing="0" width="100%" class="tableButton" style="">
+      //                           <tbody>
+      //                             <tr>
+      //                               <td style="padding-top:20px;padding-bottom:20px" align="center" valign="top">
+      //                                 <table border="0" cellpadding="0" cellspacing="0" align="center">
+      //                                   <tbody>
+      //                                     <tr>
+      //                                       <td style="background-color: rgb(0, 210, 244); padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton"> <a href="#" style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-transform:uppercase;text-decoration:none;display:block" target="_blank" class="text">Confirm Email</a>
+      //                                       </td>
+      //                                     </tr>
+      //                                   </tbody>
+      //                                 </table>
+      //                               </td>
+      //                             </tr>
+      //                           </tbody>
+      //                         </table>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="font-size:1px;line-height:1px" height="20">&nbsp;</td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td align="center" valign="middle" style="padding-bottom: 40px;" class="emailRegards">
+      //                         <!-- Image and Link // -->
+      //                         <a href="#" target="_blank" style="text-decoration:none;">
+      //                           <img mc:edit="signature" src="http://email.aumfusion.com/vespro/img//other/signature.png" alt="" width="150" border="0" style="width:100%;
+      //     max-width:150px; height:auto; display:block;">
+      //                         </a>
+      //                       </td>
+      //                     </tr>
+      //                   </tbody>
+      //                 </table>
+      //                 <table border="0" cellpadding="0" cellspacing="0" width="100%" class="space">
+      //                   <tbody>
+      //                     <tr>
+      //                       <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+      //                     </tr>
+      //                   </tbody>
+      //                 </table>
+      //               </td>
+      //             </tr>
+      //           </tbody>
+      //         </table>
+      //         <table border="0" cellpadding="0" cellspacing="0" width="100%" class="wrapperFooter" style="max-width:600px">
+      //           <tbody>
+      //             <tr>
+      //               <td align="center" valign="top">
+      //                 <table border="0" cellpadding="0" cellspacing="0" width="100%" class="footer">
+      //                   <tbody>
+      //                     <tr>
+      //                       <td style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px" align="center" valign="top" class="socialLinks">
+      //                         <a href="#facebook-link" style="display:inline-block" target="_blank" class="facebook">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/facebook.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+      //                         </a>
+      //                         <a href="#twitter-link" style="display: inline-block;" target="_blank" class="twitter">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/twitter.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+      //                         </a>
+      //                         <a href="#pintrest-link" style="display: inline-block;" target="_blank" class="pintrest">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/pintrest.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+      //                         </a>
+      //                         <a href="#instagram-link" style="display: inline-block;" target="_blank" class="instagram">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/instagram.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+      //                         </a>
+      //                         <a href="#linkdin-link" style="display: inline-block;" target="_blank" class="linkdin">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/social/light/linkdin.png" style="height:auto;width:100%;max-width:40px;margin-left:2px;margin-right:2px" width="40">
+      //                         </a>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding: 10px 10px 5px;" align="center" valign="top" class="brandInfo">
+      //                         <p class="text" style="color:#bbb;font-family:"Open Sans",Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">©&nbsp;Vespro Inc. | 800 Broadway, Suite 1500 | New York, NY 000123, USA.</p>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding: 0px 10px 20px;" align="center" valign="top" class="footerLinks">
+      //                         <p class="text" style="color:#bbb;font-family:"Open Sans",Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0"> <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">View Web Version </a>&nbsp;|&nbsp; <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">Email Preferences </a>&nbsp;|&nbsp; <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">Privacy Policy</a>
+      //                         </p>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding: 0px 10px 10px;" align="center" valign="top" class="footerEmailInfo">
+      //                         <p class="text" style="color:#bbb;font-family:"Open Sans",Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">If you have any quetions please contact us <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">support@mail.com.</a>
+      //                           <br> <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">Unsubscribe</a> from our mailing lists</p>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px" align="center" valign="top" class="appLinks">
+      //                         <a href="#Play-Store-Link" style="display: inline-block;" target="_blank" class="play-store">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/app/play-store.png" style="height:auto;margin:5px;width:100%;max-width:120px" width="120">
+      //                         </a>
+      //                         <a href="#App-Store-Link" style="display: inline-block;" target="_blank" class="app-store">
+      //                           <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/app/app-store.png" style="height:auto;margin:5px;width:100%;max-width:120px" width="120">
+      //                         </a>
+      //                       </td>
+      //                     </tr>
+      //                     <tr>
+      //                       <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+      //                     </tr>
+      //                   </tbody>
+      //                 </table>
+      //               </td>
+      //             </tr>
+      //             <tr>
+      //               <td style="font-size:1px;line-height:1px" height="30">&nbsp;</td>
+      //             </tr>
+      //           </tbody>
+      //         </table>
+      //       </td>
+      //     </tr>
+      //   </tbody>
+      // </table>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Email has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
